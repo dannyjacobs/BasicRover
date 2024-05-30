aef96b2be0855d8cbd0a672e84f66257d3cd6ace
Danny Jacobs <daniel.c.jacobs@asu.edu>
2024-05-30 11:57:39 +0100

    let influx do the timestamping

--- serial2influx.py
+++ serial2influx.py
@@ -9,29 +9,32 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 # Configure InfluxDB connection variables
 #token = "nloTTzGPJrlM_a9MwAIGz789f5Y2tEGci5GUYfRskb1GCCRTLBbCXCRHXapQk5cHu3aYAFg1E0Qf2ysY1riTmw=="
 token = "IWqHqLCDbvLwv83Hd3a66QFPwKw1m1SrFmFuIWvtzGcijDKRXuFHw6Bvk3gXd4LEJxKtD5Ly1pa2rUXphAGr3w=="
-org = "ipl"
-bucket = "rover1"
+org = "IPL"
+bucket = "rover2"
 influxip = "192.168.1.7"
-_client = InfluxDBClient(url="http://{ip}:8086/".format(ip=influxip), token=token) 
+influxurl = f"http://{influxip}:8086/"
+print("connecting to "+influxurl)
+_client = InfluxDBClient(url=influxurl, token=token, org=org) 
 _write_client = _client.write_api()
 
 #Configure the labels for the database
-measurement = "sensors"
-location = "rover"
-
+measurements = {0:"CO2",1:"Therm",2:"Sound",3:"Hum",4:"Mag",5:"Sound",6:"Therm",7:"Hum",8:"NA"}
+teams = {0:"Mongoose",1:"Pit Viper",2:"Music",3:"Panda",4:"Magneto",5:"Cat",6:"Daisy",7:"Gallery",8:"SYS"}
 try:
     ser = serial.Serial("/dev/ttyACM0")
     while True:
-        now = datetime.now()
-        timestamp_aq = datetime.timestamp(now)
-        iso = datetime.utcnow()
+        timestamp_aq = datetime.now()
         data = []
         line = str(ser.readline().strip())[2:-1]#srip off some annoying string cruft
         vals = line.split(',')
         print(f"Time: {timestamp_aq}")
         for i,val in enumerate(vals):
             L = chr(i+1+96)
-            print(f"Sensor {L}: {val}")
+            team = teams[i]
+            print(f"Sensor {L} {team}: {val}".format(team=teams[i]))
+            #letting influx timestamp this for m
+            _write_client.write(bucket, org, [{"measurement": measurements[i], "tags": {"team": teams[i]},
+                                              "fields": {"value":int(val)}}])
         time.sleep(1)
 except KeyboardInterrupt:
     _write_client.__del__()
